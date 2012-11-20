6af9cc1c9693a85ffd700438eeb9cf3bef6fc7fb
Taha Azab <azab.taha@gmail.com>
2012-11-20 02:33:07 +0200

    demonstrated Pluggable Adapter Pattern

---
+++ AdapterPattern/PluggableAdapterPattern.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    // Adapter Pattern - Pluggable Judith Bishop Oct 2007
+    // Adapter can accept any number of pluggable adaptees and targets
+    // and route the requests via a delegate, in some cases using the
+    // anonymous delegate construct
+
+    // Existing way requests are implemented
+    internal class Adaptee2
+    {
+        public double Precise(double a, double b)
+        {
+            return a/b;
+        }
+    }
+
+    // New standard for requests
+    internal class Target
+    {
+        public string Estimate(int i)
+        {
+            return "Estimate is " + (int) Math.Round(i/3.0);
+        }
+    }
+
+    // Implementing new requests via old
+    internal class Adapter2 : Adaptee2
+    {
+        public Func<int, string> Request;
+
+        // Different constructors for the expected targets/adaptees
+
+        // Adapter-Adaptee
+        public Adapter2(Adaptee2 adaptee)
+        {
+            // Set the delegate to the new standard
+            Request = delegate(int i)
+                          {
+                              return string.Format("Estimate based on precision is {0}", (int) Math.Round(Precise(i, 3)));
+                          };
+        }
+
+        // Adapter-Target
+        public Adapter2(Target target)
+        {
+            // Set the delegate to the existing standard
+            Request = target.Estimate;
+        }
+    }
+
+    internal class Client
+    {
+        private static void Main3()
+        {
+
+            Adapter2 adapter1 = new Adapter2(new Adaptee2());
+            Console.WriteLine(adapter1.Request(5));
+
+            Adapter2 adapter2 = new Adapter2(new Target());
+            Console.WriteLine(adapter2.Request(5));
+
+        }
+    }
+}
